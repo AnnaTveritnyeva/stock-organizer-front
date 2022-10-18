9f063d4abdd1559ab3df16ccf126fd2a9785f950
AnnaTveritnyeva <atveretniv@gmail.com>
2022-10-18 13:29:15 +0300

    Updated header and added class and page to Tabs

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,34 +1,34 @@
 import React, { useState } from 'react';
-import { Box, Tab, Typography } from '@mui/material';
+import { Box, Tab } from '@mui/material';
 import TabPanel from '@mui/lab/TabPanel';
 import { TabContext, TabList } from '@mui/lab';
 import Lists from './Lists';
+import './Main.modules.css'
+import Logo from '../assets/Logo.png'
 
 function Main() {
-    const [page, setPage] = useState("Lists")
-    const pages = ["Lists", "To Buy"]
-
+    const [page, setPage] = useState("Categories")
+    const pages = ["Categories", "To Buy", "Stores"]
 
     const handleChange = (event, newValue) => {
         setPage(newValue);
     };
 
     return (
-        <Box sx={{ width: '100%' }}>
-            <Typography variant='h2'>
-                What To Buy?
-            </Typography>
+        <Box className='Main' sx={{ width: '100%' }}>
+            <div className='header'>
+                <img src={Logo} height={'300px'} />
+            </div>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <TabContext value={page}>
                     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                         <TabList variant="fullWidth" onChange={handleChange} aria-label="lab API tabs example">
-                            <Tab label={pages[0]} value={pages[0]} />
-                            <Tab label={pages[1]} value={pages[1]} />
+                            <Tab className={"tab"} label={pages[0]} value={pages[0]} />
+                            <Tab className={"tab"} label={pages[1]} value={pages[1]} />
+                            <Tab className={"tab"} label={pages[2]} value={pages[2]} />
                         </TabList>
                     </Box>
-                    <TabPanel value={pages[0]}>
-                        <Lists
-                        />
+                    <TabPanel value={pages[0]}> <Lists />
                     </TabPanel>
                     <TabPanel value={pages[1]}>{pages[1]}</TabPanel>
                 </TabContext>
