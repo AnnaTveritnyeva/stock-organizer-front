ca1d3879d5366be976500d2ec597eb9198ad566b
AnnaTveritnyeva <atveretniv@gmail.com>
2022-10-18 13:22:54 +0300

    Updated App.js to fetch data from server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Main from './components/Main.js';
+import { initializeCategoriesFromServer, initializeItemsFromServer } from 'redux/ListsSlice';
+import CircularProgress from '@mui/material/CircularProgress';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+
+  function getServerItemsData() {
+    axios.get("http://localhost:8080/items/getAllItems")
+      .then(response => {
+        dispatch(initializeItemsFromServer(response.data))
+      })
+      .catch(err => { console.log(err) });
+  }
+
+  function getCategoriesData() {
+    axios.get("http://localhost:8080/categories/getAllCategories")
+      .then(response => {
+        dispatch(initializeCategoriesFromServer(response.data))
+        setLoading(false)
+      })
+      .catch(err => { console.log(err) });
+  }
+
+  function getDataFromServer() {
+    getServerItemsData();
+    getCategoriesData();
+  }
+
+  useEffect(() => {
+    getDataFromServer();
+  }, [])
+
   return (
+
     <div>
-      <Main />
+      {loading ? <CircularProgress /> : <Main />}
     </div>
 
   )
