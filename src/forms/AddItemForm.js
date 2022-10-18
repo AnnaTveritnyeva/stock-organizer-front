ddda524bd636b0f05f9a1a2244d5ba85ddfe00f8
AnnaTveritnyeva <atveretniv@gmail.com>
2022-10-18 13:37:26 +0300

    Created AddItemForm.js

---
+++ src/forms/AddItemForm.js
@@ -0,0 +1,39 @@
+import { Button, Checkbox, TextField, Typography } from '@mui/material';
+import { useForm } from "react-hook-form";
+import CircleIcon from '@mui/icons-material/Circle';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addItem } from 'redux/ListsSlice';
+import axios from 'axios';
+
+function AddItemForm(props) {
+    const { register, setValue, handleSubmit } = useForm();
+    const dispatch = useDispatch()
+    const [missing, setMissing] = useState(false)
+
+    const onSubmit = (data) => {
+        axios.post("http://localhost:8080/items/addItem", data)
+            .then(response => dispatch(addItem(response.data)))
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Typography> New Item: </Typography>
+
+            <Typography> Name: </Typography>
+            <TextField variant="outlined" {...register("name", { required: true })} required />
+            <Typography> missing </Typography>
+            <Checkbox onChange={() => setMissing(!missing)} />
+            {setValue("missing", missing)}
+            {setValue("organizationHierarchy", { categoryName: props.category.name, sectionName: props.section.name })}
+            {setValue("purchaseHistory", [])}
+            {/* onClick = should close. 
+                todo: figure out a way to pass the data to the Modal component */}
+            <Button > Cancel </Button>
+            <Button type="submit"> Save </Button>
+        </form>
+    );
+}
+
+export default AddItemForm;
