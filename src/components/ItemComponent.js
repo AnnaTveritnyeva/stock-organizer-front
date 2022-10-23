72cc5a4628ca626538c13a5dae46cd9d0600fbcb
AnnaTveritnyeva <atveretniv@gmail.com>
2022-10-23 17:56:20 +0300

    Added popper to display item info

--- src/components/ItemComponent.js
+++ src/components/ItemComponent.js
@@ -3,15 +3,19 @@ import './ItemComponent.modules.css'
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import { Checkbox, Typography } from "@mui/material";
+import { Checkbox, ClickAwayListener, Popper, Typography } from "@mui/material";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { updateItem } from "redux/ListsSlice";
+import ItemInfoPopper from "./ItemInfoPopper";
 
 
 function ItemComponent(props) {
     const dispatch = useDispatch()
     const [item, setItem] = useState(props.item)
+
+    const [open, setOpen] = useState(false)
+    const [anchorEl, setAnchorEl] = useState(null);
 
     const handleChangeMissing = () => {
         axios.put("http://localhost:8080/items/toggleMissing", item)
@@ -20,6 +24,16 @@ function ItemComponent(props) {
                 setItem(response.data)
             })
             .catch(err => console.log(err))
+    }
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null)
+        setOpen(false)
     }
 
     return (
@@ -39,9 +53,15 @@ function ItemComponent(props) {
             <div className="item" >
                 <Typography sx={{ borderBottom: '1.5px solid black', width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                     <span >{item.name}</span>
-                    <InfoOutlinedIcon sx={{ color: 'rgb(102,102,102)', fontSize: 'inherit' }} />
+                    <InfoOutlinedIcon sx={{ color: 'rgb(102,102,102)', fontSize: 'inherit' }} onClick={handleClick} />
                 </Typography>
             </div>
+
+            <Popper open={open} anchorEl={anchorEl} placement={'right'} >
+                <ClickAwayListener onClickAway={handleClose}>
+                    <ItemInfoPopper item={item} />
+                </ClickAwayListener>
+            </Popper>
         </div>
     )
 }
