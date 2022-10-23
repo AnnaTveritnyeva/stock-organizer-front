03b32a35500c679fde44603ad942ea558d16e5d8
AnnaTveritnyeva <atveretniv@gmail.com>
2022-10-23 17:52:54 +0300

    Created theme.js,  conncted it and changed font

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store'
+import { responsiveFontSizes, ThemeProvider } from '@mui/material';
+import { theme } from './theme';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+<Provider store={store}>
   <React.StrictMode>
+  <ThemeProvider theme={responsiveFontSizes(theme)}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    typography: {
+        fontFamily: 'Balsamiq Sans',
+        fontWeightLight: '300',
+        fontWeightRegular: '400',
+        fontWeightMedium: '700',
+        fontWeightBold: '800'
+    },
+});
