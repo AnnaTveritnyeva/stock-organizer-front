59b203836a9bb1e79fac76a48df7447a1d8a6802
AnnaTveritnyeva <atveretniv@gmail.com>
2022-11-05 20:48:47 +0200

    Added item edit handling and removed location info

--- src/components/ItemInfoPopper.js
+++ src/components/ItemInfoPopper.js
@@ -1,41 +1,64 @@
-import { Link, Typography } from "@mui/material";
-import React from "react";
+import { ClickAwayListener, Link, Typography } from "@mui/material";
+import React, { useState } from "react";
 import './ItemInfoPopper.modules.css'
 import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from '@mui/icons-material/Add';
+import UpdateItemForm from "forms/UpdateItemForm";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { updateItem } from "redux/ListsSlice";
 
 function ItemInfoPopper(props) {
     const item = props.item;
-    return (
-        <div className="item-info-container">
-            <div className="arrow-left">
+    const dispatch = useDispatch()
+    const [editMode, setEditMode] = useState(false)
 
+    function onSubmitUpdateItem(data) {
+        axios.post("http://localhost:8080/items/updateItem", data)
+            .then(response => dispatch(updateItem({ toUpdate: item, updated: response.data })))
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <ClickAwayListener onClickAway={props.handleClose}>
+            <div className="item-info-container">
+                <div className="arrow-left"> </div>
+                {editMode ? <UpdateItemForm onSubmitUpdateItem={onSubmitUpdateItem} item={item} handleClose={props.handleClose} /> :
+                    <div className="item-info">
+                        <div className="item-info-title">
+                            <Typography variant="h5" fontWeight="bold">
+                                {item.name}
+                            </Typography>
+                            <EditIcon onClick={() => setEditMode(true)} />
+                        </div>
+                        <div className="item-info-section-div">
+                            <div className="item-info-section-title">
+                                <Typography>
+                                    Where To Find?
+                                </Typography>
+                                <AddIcon sx={{ fontSize: '16px' }} />
+                            </div>
+
+                            {item.storeURLs.length > 0 ? item.storeURLs.map(store =>
+                                <Link key={item.id + store.url} href={store.url} target="_blank">{store.name}</Link>
+                            ) : <Typography>No stores added yet</Typography>}
+                        </div>
+                        <div className="item-info-section-div">
+                            <div className="item-info-section-title">
+                                <Typography>
+                                    Purchase History
+                                </Typography>
+                                <AddIcon sx={{ fontSize: '16px' }} />
+                            </div>
+                            {item.purchaseHistory.length > 0 ? item.purchaseHistory.map(date =>
+                                <Typography key={date}>
+                                    {new Date(date).toLocaleDateString('en-GB')}
+                                </Typography>
+                            ) : <Typography>No purchase history </Typography>}
+                        </div>
+                    </div>}
             </div>
-            <div className="item-info">
-                <Typography variant="h5" fontWeight="bold">
-                    {item.name}
-                </Typography>
-                <div className="item-location-container">
-                    <div>
-                        {item.organizationHierarchy.categoryName} > {item.organizationHierarchy.sectionName}
-                    </div>
-                    <EditIcon sx={{ fontSize: "16px" }} />
-                </div>
-                <Typography variant="h6">
-                    Where To Find?
-                </Typography>
-                {item.storeURLs.length > 0 ? item.storeURLs.map(store =>
-                    <Link key={item.id + store.url} href={store.url} target="_blank">{store.name}</Link>
-                ) : <Typography>No stores added yet</Typography>}
-                <Typography variant="h6">
-                    Purchase History
-                </Typography>
-                {item.purchaseHistory.length > 0 ? item.purchaseHistory.map(date =>
-                    <Typography>
-                        {new Date(date).toLocaleDateString('en-GB')}
-                    </Typography>
-                ) : <Typography>No purchase history </Typography>}
-            </div>
-        </div>
+        </ClickAwayListener>
     )
 }
 
